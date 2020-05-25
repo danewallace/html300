753e7b18cc01138b690781f5ed583a505b6010e0
Dane Wallace <danewallace@icloud.com>
2020-05-25 16:41:45 -0700

    Added first function to create initial array

--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -1,26 +1,36 @@
 // jQuery Doc Ready
-$(function () {
-    // Set up some data and variables
-    const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultSum = $('.sum');
-    // Click event on the submit input
-    submit.on('click', function () {
-        // Grab the input value and parse it into a number
-        const val = input.val();
-        const num = parseInt(val);
-        // Verify the user gave us a real number
-        if (!isNaN(num)) {
-            // Add our new number to the data array
-            data.push(num);
-            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-            const sum = data.reduce((acc, curr) => acc + curr);
-            // Add our new total to the page
-            resultSum.text(sum);
-            // Reset the input value to an empty string
-            input.val('');
-        }
-    });
-});
+// $(function () {
+//     // Set up some data and variables
+//     const data = [1, 2, 3, 4],
+//         input = $('.input'),
+//         submit = $('.submit'),
+//         resultSum = $('.sum');
+//     // Click event on the submit input
+//     submit.on('click', function () {
+//         // Grab the input value and parse it into a number
+//         const val = input.val();
+//         const num = parseInt(val);
+//         // Verify the user gave us a real number
+//         if (!isNaN(num)) {
+//             // Add our new number to the data array
+//             data.push(num);
+//             // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+//             const sum = data.reduce((acc, curr) => acc + curr);
+//             // Add our new total to the page
+//             resultSum.text(sum);
+//             // Reset the input value to an empty string
+//             input.val('');
+//         }
+//     });
+// });
 
+let initialData = [10, 16, 36, 37, 57, 78];
+initialArray = $('.initialArray');
+
+$(function() {
+    initialData.forEach(function(value, key) {
+    console.log(`Key: ${key} - Value: ${value}`);
+    initialArray.append(`Key: ${key} `);
+    initialArray.append(`- Value: ${value}<br>`);
+  })
+})
