ed319e0ef42d2388eaa8ab23e556f50b9037770d
Dane Wallace <danewallace@icloud.com>
2020-05-26 11:28:38 -0700

    Minor formatting adjustments to make forEach consistent with other sections in index.html

--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -1,30 +1,9 @@
 // jQuery Doc Ready
-// $(function () {
-//     // Set up some data and variables
-//     const data = [1, 2, 3, 4],
-//         input = $('.input'),
-//         submit = $('.submit'),
-//         resultSum = $('.sum');
-//     // Click event on the submit input
-//     submit.on('click', function () {
-//         // Grab the input value and parse it into a number
-//         const val = input.val();
-//         const num = parseInt(val);
-//         // Verify the user gave us a real number
-//         if (!isNaN(num)) {
-//             // Add our new number to the data array
-//             data.push(num);
-//             // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-//             const sum = data.reduce((acc, curr) => acc + curr);
-//             // Add our new total to the page
-//             resultSum.text(sum);
-//             // Reset the input value to an empty string
-//             input.val('');
-//         }
-//     });
-// });
 $(function() {
+  // Declare original array
   let initialData = [3, 6, 9, 10, 11, 20, 37, 45, 56, 89];
+
+  // Declare variables based on jQuery selectors
   initialArray = $('.initialArray');
   filterInput = $('.filterInput');
   filterSubmit = $('.filterSubmit');
@@ -36,12 +15,12 @@ $(function() {
   includesInput = $('.includesInput');
   includesOutput = $('.includesOutput');
 
-    // set the initial array at the top of the page
-    initialData.forEach(function(value, key) {
-      initialArray.append(`Key: ${key} - Value: ${value}<br> `);
-    });
+  // Set the initial array at the top of the page
+  initialData.forEach(function(value, key) {
+    initialArray.append(`Key: ${key} - Value: ${value}<br> `);
+  });
 
-  // filter function
+  // Filter function
   filterSubmit.on('click', function() {
     // Console log button click for debugging
     console.log('functionSubmit clicked');
@@ -55,14 +34,14 @@ $(function() {
       let filterResults = initialData.filter(value => value > filterVal);
       // Creates new array and appends it to index.html section
       filterResults.forEach(function(value, key) {
-          filterArray.append(`New key ${key} with a value of ${value} is greater than ${filterVal} <br>`);
-        });
+        filterArray.append(`New key ${key} with a value of ${value} is greater than ${filterVal} <br>`);
+      });
       // Clears the input box
       filterInput.val('');
     }
   });
 
-  // map function
+  // Map function
   mapSubmit.on('click', function() {
     // Console log button click for debugging
     console.log('mapSubmit clicked');
@@ -76,14 +55,14 @@ $(function() {
       let mapResults = initialData.map(value => value + mapVal);
       // Creates new array and appends it to index.html section
       mapResults.forEach(function(value, key) {
-          mapArray.append(`Key ${key} has a new value of ${value} after adding ${mapVal}<br>`);
-        });
+        mapArray.append(`Key ${key} has a new value of ${value} after adding ${mapVal}<br>`);
+      });
       // Clears the input box
       mapInput.val('');
     }
   });
 
-  // includes function
+  // Includes function
   includesSubmit.on('click', function() {
     const originalString = "Who's the more foolish, the fool, or the fool who follows him?";
     // Console log button click for debugging
@@ -96,6 +75,32 @@ $(function() {
     // Clears the input box
     includesInput.val('');
   });
+
+ // Sample content
+ //  $(function () {
+ //     // Set up some data and variables
+ //     const data = [1, 2, 3, 4],
+ //         input = $('.input'),
+ //         submit = $('.submit'),
+ //         resultSum = $('.sum');
+ //     // Click event on the submit input
+ //     submit.on('click', function () {
+ //         // Grab the input value and parse it into a number
+ //         const val = input.val();
+ //         const num = parseInt(val);
+ //         // Verify the user gave us a real number
+ //         if (!isNaN(num)) {
+ //             // Add our new number to the data array
+ //             data.push(num);
+ //             // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+ //             const sum = data.reduce((acc, curr) => acc + curr);
+ //             // Add our new total to the page
+ //             resultSum.text(sum);
+ //             // Reset the input value to an empty string
+ //             input.val('');
+ //         }
+ //     });
+ // });
 
 
 });
