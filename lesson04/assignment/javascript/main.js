c5ff5ce8fe5deb44e215709de1733a33a7cdf19b
Dane Wallace <danewallace@icloud.com>
2020-07-29 21:04:47 -0700

    Updated class titles and refined justification of art

--- lesson04/assignment/javascript/main.js
+++ lesson04/assignment/javascript/main.js
@@ -1,4 +1,5 @@
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
+//Listener to toggle art flipping behavior for cover art
+var art = document.querySelector('.art');
+art.addEventListener( 'click', function() {
+  art.classList.toggle('is-flipped');
 });
