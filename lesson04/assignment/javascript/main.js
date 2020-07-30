a598e20b9b96f13e414914c8f1163cfc0c984642
Dane Wallace <danewallace@icloud.com>
2020-07-30 12:20:07 -0700

    Beautified index.html and main.js

--- lesson04/assignment/javascript/main.js
+++ lesson04/assignment/javascript/main.js
@@ -2,6 +2,6 @@
 // Original source code only functioned for a single element of the class
 
 // Toggles is-flipped class on each playlist cover
-$('.art').click(function(){
+$('.art').click(function() {
   $(this).toggleClass('is-flipped');
 });
