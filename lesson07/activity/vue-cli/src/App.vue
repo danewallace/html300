054ce3f975c882774814c1b59dcfcb2951114d2e
Dane Wallace <danewallace@icloud.com>
2020-08-22 10:33:36 -0700

    Updated tooltip to be disabled when ClickMe clicked

--- lesson07/activity/vue-cli/src/App.vue
+++ lesson07/activity/vue-cli/src/App.vue
@@ -35,24 +35,10 @@
         <b-button type="submit" style="pointer-events: none;" :disabled="disabled">Submit</b-button>
       </span>
       <!-- Tooltip -->
-      <b-tooltip target="disabled-wrapper">Click the other button</b-tooltip>
+      <b-tooltip target="disabled-wrapper" :disabled="!disabled">Click the other button</b-tooltip>
     </div>
   </b-form>
 
-  <!-- <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
 </div>
 </template>
 
@@ -64,6 +50,7 @@ export default {
       msg: 'Hello',
       link: "https://www.google.com/",
       counter: 0,
+      toolTipShow: true,
       show: false,
       ingredients: ["meat", "fruit", "vegetables"],
       persons: [{
