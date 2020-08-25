d6c67b4ca95728b1b29684e09f16203a7b52a606
Dane Wallace <danewallace@icloud.com>
2020-08-25 09:38:50 -0700

    Replaced About vue with Index.html components in assignment06 and correctly configured routes

---
+++ lesson07/assignment/vue-assignment/src/views/Landing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="landing">
+    <!-- Bootstrap navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="index.html"><span style='font-size:100px;'>&#128049;</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="images.html">Images</a>
+          <a class="nav-item nav-link" href="facts.html">Facts</a>
+          <a class="nav-item nav-link" href="social.html">Social</a>
+        </div>
+      </div>
+    </nav>
+
+  <!-- Bootstrap jumbotron for main content -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"><span>&#128075;</span></h1>
+        <p class="lead">This assignment is an introduction to Bootstrap. Click the links above to explore some features.</p>
+      </div>
+    </div>
+  </div>
+</template>
