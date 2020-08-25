1be6afa3ab1bf53206a8a36161c0c9863a209db0
Dane Wallace <danewallace@icloud.com>
2020-08-25 11:20:50 -0700

    Implemented router link for home nav icon

--- lesson07/assignment/vue-assignment/src/components/Images.vue
+++ lesson07/assignment/vue-assignment/src/components/Images.vue
@@ -2,7 +2,7 @@
   <div class="images">
     <!-- Bootstrap navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.html"><span style='font-size:100px;'>&#128049;</span></a>
+      <router-link class="navbar-brand" to="/"><span style='font-size:100px;'>&#128049;</span></router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
