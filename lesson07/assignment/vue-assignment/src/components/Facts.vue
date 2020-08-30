f7fa728cf9291788adb066946c4d331a3368877c
Dane Wallace <danewallace@icloud.com>
2020-08-30 12:39:20 -0700

    Refined dynamic toggle

--- lesson07/assignment/vue-assignment/src/components/Facts.vue
+++ lesson07/assignment/vue-assignment/src/components/Facts.vue
@@ -23,9 +23,10 @@
     <!-- Looped accordion card -->
     <b-card no-body class="mb-1" v-for="(fact, index) in facts" :key="fact.title">
       <b-card-header header-tag="header" class="p-1" role="tab" >
-        <b-button block v-b-toggle.accordion-1 variant="info"> {{ fact.title }}</b-button>
+        <b-button block v-b-toggle="'accordion-' + index"  variant="info"> {{ fact.title }}</b-button>
+        <!-- <b-button block v-b-toggle.accordion-1 variant="info"> {{ fact.title }}</b-button> -->
       </b-card-header>
-      <b-collapse v-bind:id="'accordion'+index" accordion="my-accordion" role="tabpanel">
+      <b-collapse v-bind:id="'accordion-'+index" accordion="my-accordion" role="tabpanel">
         <b-card-body>
           <b-card-text>{{ fact.body }}</b-card-text>
         </b-card-body>
