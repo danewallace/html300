f0d81f229b3a263049b2abca43879cf873b59ac6
Dane Wallace <danewallace@icloud.com>
2020-08-30 13:17:21 -0700

    Corrected nav bar collapse on home component

--- lesson07/assignment/vue-assignment/src/components/Home.vue
+++ lesson07/assignment/vue-assignment/src/components/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="home">
-  <!-- Bootstrap vue navbar -->
-  <b-navbar toggleable="sm" type="light" variant="light">
-   <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+    <!-- Bootstrap vue navbar -->
+    <b-navbar toggleable="sm" type="light" variant="light">
+     <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-   <b-navbar-brand><router-link class="navbar-brand" to="/"><span style='font-size:100px;'>&#128049;</span></router-link></b-navbar-brand>
+     <b-navbar-brand><router-link class="navbar-brand" to="/"><span style='font-size:100px;'>&#128049;</span></router-link></b-navbar-brand>
 
-   <b-collapse id="nav-text-collapse" is-nav>
-     <b-navbar-nav>
-       <router-link class="nav-item nav-link active" to="/">Home<span class="sr-only">(current)</span></router-link>
-        <router-link class="nav-item nav-link" to="/images">Images</router-link>
-        <router-link class="nav-item nav-link" to="/facts">Facts</router-link>
-        <router-link class="nav-item nav-link" to="/social">Social</router-link>
-     </b-navbar-nav>
-   </b-collapse>
- </b-navbar>
+     <b-collapse id="nav-text-collapse" is-nav>
+       <b-navbar-nav>
+         <router-link class="nav-item nav-link active" to="/">Home<span class="sr-only">(current)</span></router-link>
+          <router-link class="nav-item nav-link" to="/images">Images</router-link>
+          <router-link class="nav-item nav-link" to="/facts">Facts</router-link>
+          <router-link class="nav-item nav-link" to="/social">Social</router-link>
+       </b-navbar-nav>
+     </b-collapse>
+   </b-navbar>
 
   <!-- Bootstrap jumbotron for main content -->
     <div class="jumbotron jumbotron-fluid">
