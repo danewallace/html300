7a9e11f7ad7f104300cf7cbae4653d06a1f7d046
Dane Wallace <danewallace@icloud.com>
2020-08-30 15:16:08 -0700

    Implemented v-if v-else button toggle to control image on Social

--- lesson07/assignment/vue-assignment/src/components/Social.vue
+++ lesson07/assignment/vue-assignment/src/components/Social.vue
@@ -1,50 +1,55 @@
 <template>
-  <div class="social">
-    <!-- Bootstrap vue navbar -->
-    <b-navbar toggleable="sm" type="light" variant="light">
-     <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+<div class="social">
+  <!-- Bootstrap vue navbar -->
+  <b-navbar toggleable="sm" type="light" variant="light">
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-     <b-navbar-brand><router-link class="navbar-brand" to="/"><span style='font-size:100px;'>&#128049;</span></router-link></b-navbar-brand>
+    <b-navbar-brand>
+      <router-link class="navbar-brand" to="/"><span style='font-size:100px;'>&#128049;</span></router-link>
+    </b-navbar-brand>
 
-     <b-collapse id="nav-text-collapse" is-nav>
-       <b-navbar-nav>
-         <router-link class="nav-item nav-link" to="/">Home<span class="sr-only">(current)</span></router-link>
-          <router-link class="nav-item nav-link" to="/images">Images<span class="sr-only">(current)</span></router-link>
-          <router-link class="nav-item nav-link" to="/facts">Facts</router-link>
-          <router-link class="nav-item nav-link active" to="/social">Social<span class="sr-only">(current)</span></router-link>
-       </b-navbar-nav>
-     </b-collapse>
-   </b-navbar>
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav>
+        <router-link class="nav-item nav-link" to="/">Home<span class="sr-only">(current)</span></router-link>
+        <router-link class="nav-item nav-link" to="/images">Images<span class="sr-only">(current)</span></router-link>
+        <router-link class="nav-item nav-link" to="/facts">Facts</router-link>
+        <router-link class="nav-item nav-link active" to="/social">Social<span class="sr-only">(current)</span></router-link>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 
   <!-- Page heading -->
-    <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0" style="margin-left: 1rem;"> {{ title }} </h1>
+  <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0" style="margin-left: 1rem;"> {{ title }} </h1>
 
-    <!-- will render only 'ELSE' -->
-<div>
-    <h2 v-if="b">IF</h2>
-    <h2 v-else>ELSE</h2>
-</div>
 
-    <!-- Bootstrap grid declaration -->
-    <div class="row">
-      <!-- Bootstrap grid item for page image -->
-      <div class="col-xl-8 col-lg-7 col-sm-6 col-12 p-5">
-        <img v-bind:src="source" alt="..." class="img-fluid">
-      </div>
-      <!-- Bootstrap grid item for page copy -->
-      <div class="col-xl-4 col-lg-5 col-sm-6 col-12 p-5">
-        <p class="lead text-center"> {{ heading }} </p>
-        <p class="text-center"> {{ msg }} </p>
-        <b-button pill v-bind:href="link">{{ btnText }}</b-button>
-      </div>
+  <!-- Bootstrap grid declaration -->
+  <div class="row">
+    <div class="col-xl-8 col-lg-7 col-sm-6 col-12 p-5" v-if="kittenMode">
+      <img v-bind:src="kittenSource" alt="..." class="img-fluid">
+      <p></p>
+      <b-button variant="outline-primary" v-on:click="kittenMode = !kittenMode">Toggle Kitten Mode</b-button>
+    </div>
+    <div class="col-xl-8 col-lg-7 col-sm-6 col-12 p-5" v-else>
+      <img v-bind:src="source" alt="..." class="img-fluid">
+      <p></p>
+      <b-button variant="outline-primary" v-on:click="kittenMode = !kittenMode">Toggle Kitten Mode</b-button>
+    </div>
+
+    <!-- Bootstrap grid item for page copy -->
+    <div class="col-xl-4 col-lg-5 col-sm-6 col-12 p-5">
+      <p class="lead text-center"> {{ heading }} </p>
+      <p class="text-center"> {{ msg }} </p>
+      <b-button pill v-bind:href="link">{{ btnText }}</b-button>
     </div>
   </div>
+
+</div>
 </template>
 
 <script>
 export default {
   name: "Social",
-  data () {
+  data() {
 
     return {
       title: "Social",
@@ -52,10 +57,9 @@ export default {
       msg: "They're a long-haired tuxedo cat and Scottish Fold that grew up in New York City and currently reside in sunny Los Angeles, CA.",
       btnText: "Follow on Instagram",
       source: 'https://p46.f4.n0.cdn.getcloudapp.com/items/o0uxGLvW/moonandmiumiu.jpeg?v=cd58ce8822e2316f57d9e648a389acef',
+      kittenSource: 'https://p46.f4.n0.cdn.getcloudapp.com/items/8LuPg7Kz/IMG_0187.JPG?source=viewer&v=805dcc1c7bd11a1afcec4c3adbe0f9c9',
       link: 'https://www.instagram.com/moonandmiumiu/',
-      a: true,
-      b: false
-
+      kittenMode: false
     }
   }
 };
