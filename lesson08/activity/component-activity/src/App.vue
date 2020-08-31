e6fccdcda03cb29d081485730036106412a7d8ac
Dane Wallace <danewallace@icloud.com>
2020-08-30 20:06:33 -0700

    Created Post.vue and copied objects from data.js into App.vue

--- lesson08/activity/component-activity/src/App.vue
+++ lesson08/activity/component-activity/src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
@@ -24,7 +9,12 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      posts: [
+        	{ id: 1, author: 'Robin Rendle', title: 'A Conspiracy to Kill IE6', likes: 10, category: 'browsers', excerpt: 'Chris Zacharias published a few notes about why the team at YouTube added a banner that asked users to switch from IE6 to a more modern browser back in 2009' },
+        	{ id: 2, author: 'Raymond Camden', title: 'Making the Move from jQuery to Vue', likes: 15, category: 'vue', excerpt: 'As someone who has used jQuery for many. years and has recently become a Vue convert, I thought it would be an interesting topic to discuss the migration process of working with one to the other.' },
+        	{ id: 3, author: 'Chris Coyier', title: 'Naming things to improve accessibility', likes: 13, category: 'accessibility', excerpt: 'In modern browsers, our markup becomes an accessibility tree that ultimately informs what our interface looks like to assistive technologies. It doesn’t matter as much whether you’ve written this markup' },
+        	{ id: 4, author: 'Dan Englishby', title: 'The Many Ways of Getting Data Into Charts', likes: 9, category: 'apis', excerpt: 'Data is available everywhere nowadays, whether it’s in a plain text file, a REST API, an online Google sheet... you name it! It’s that variety of context that makes building graphs more than simply having a database in your local project — where there is data, there is a way.' }
+      ]
     }
   }
 }
