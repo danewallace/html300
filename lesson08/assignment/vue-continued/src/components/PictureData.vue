3e4e21cab07b025454453fef7fa6b2031b11c1c4
Dane Wallace <danewallace@icloud.com>
2020-09-01 16:05:57 -0700

    Refining prop data passing

--- lesson08/assignment/vue-continued/src/components/PictureData.vue
+++ lesson08/assignment/vue-continued/src/components/PictureData.vue
@@ -1,7 +1,7 @@
 <template>
  <div id='picture-data'>
-   <a v-bind:href="link" target="_blank" class="d-block mb-4 h-100">
-     <img class="img-fluid img-thumbnail" v-bind:src="source" alt="alt">
+   <a href="link" target="_blank" class="d-block mb-4 h-100">
+     <img class="img-fluid img-thumbnail" :src="source" :alt="alt">
    </a>
  </div>
 </template>
